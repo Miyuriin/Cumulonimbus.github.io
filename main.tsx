1c700a481ee2be428667a085c96668902658ce10
Francisco Santos <francisco.santos@ahrefs.com>
2023-07-02 15:09:08 +0100

    first commit

---
+++ main.tsx
@@ -0,0 +1,17 @@
+/** @jsx h */
+
+import * as A from "https://esm.sh/marked@5.1.0";
+// import blog from "blog";
+
+// blog({
+//   title: "Margarida Fernandes",
+//   author: "Margarida Fernandes",
+//   avatar: "./assets/LunaLang2.jpg",
+//   avatarClass: "full",
+//   links: [
+//     { title: "Email", url: "margaridafernandes012@gmail.com" },
+//     { title: "Instagram", url: "https://www.instagram.com/sf.margarida" },
+//   ],
+//   // background: "#fff",
+//   theme: "dark"
+// });
