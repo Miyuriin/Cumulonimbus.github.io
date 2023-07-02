b7a7caf9569ee2125a64370856ce280f5b9941cf
Francisco Santos <francisco.santos@ahrefs.com>
2023-07-02 15:28:41 +0100

    image

--- main.tsx
+++ main.tsx
@@ -5,12 +5,11 @@ import blog from "blog";
 blog({
   title: "Cumulonimbus",
   author: "Margarida Fernandes",
-  avatar: "/assets/LunaLang2.jpg",
+  avatar: "./avatar.jpg",
   avatarClass: "full",
   links: [
     { title: "Email", url: "mailto:margaridafernandes012@gmail.com" },
     { title: "Instagram", url: "https://www.instagram.com/sf.margarida" },
   ],
-  // background: "#fff",
   theme: "dark",
 });
