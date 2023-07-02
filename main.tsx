ac2dab8fb39322826cab42733a369dabbf082e4e
Francisco Santos <francisco.santos@ahrefs.com>
2023-07-02 15:19:31 +0100

    second

--- main.tsx
+++ main.tsx
@@ -1,17 +1,16 @@
 /** @jsx h */
 
-import * as A from "https://esm.sh/marked@5.1.0";
-// import blog from "blog";
+import blog from "blog";
 
-// blog({
-//   title: "Margarida Fernandes",
-//   author: "Margarida Fernandes",
-//   avatar: "./assets/LunaLang2.jpg",
-//   avatarClass: "full",
-//   links: [
-//     { title: "Email", url: "margaridafernandes012@gmail.com" },
-//     { title: "Instagram", url: "https://www.instagram.com/sf.margarida" },
-//   ],
-//   // background: "#fff",
-//   theme: "dark"
-// });
+blog({
+  title: "Margarida Fernandes",
+  author: "Margarida Fernandes",
+  avatar: "./assets/LunaLang2.jpg",
+  avatarClass: "full",
+  links: [
+    { title: "Email", url: "margaridafernandes012@gmail.com" },
+    { title: "Instagram", url: "https://www.instagram.com/sf.margarida" },
+  ],
+  // background: "#fff",
+  theme: "dark",
+});
